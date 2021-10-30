f90a720dc6f9f4f14823d3ee3caba1b516d82178
AbrhamAsfaw <abrhamasfaw2018@gmail.com>
2021-10-30 16:40:36 +0300

    Resolved Save Employee Issue

--- src/main/java/com/abrham/Employee/Management/System/controller/EmployeeController.java
+++ src/main/java/com/abrham/Employee/Management/System/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.abrham.Employee.Management.System.controller;
 
+import com.abrham.Employee.Management.System.model.Employee;
 import com.abrham.Employee.Management.System.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -14,7 +17,21 @@ public class EmployeeController {
 
     @GetMapping("/")
     public String viewHomePage(Model model){
-        model.addAttribute("listEmployees" , employeeService.getAllEmployees());
+        model.addAttribute("employees" , employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/newEmployeeForm")
+    public String newEmployeeForm(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee" , employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+
     }
 }
